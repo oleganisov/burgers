bfbd06194ee1919e01aa4bcd166ce76c581ecbd4
Oleg V. Anisov <anisov@obninsk.ru>
2018-11-05 23:15:05 +0300

    first we reviews section

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,10 @@
-var syntax        = 'scss'; // Syntax: sass or scss;
-
-var gulp          = require('gulp'),
-	gutil         = require('gulp-util' ),
+var gulp        = require('gulp'),
 	sass          = require('gulp-sass'),
 	browsersync   = require('browser-sync'),
 	concat        = require('gulp-concat'),
-	uglify        = require('gulp-uglify'),
-	cleancss      = require('gulp-clean-css'),
-	rename        = require('gulp-rename'),
 	autoprefixer  = require('gulp-autoprefixer'),
 	notify        = require("gulp-notify"),
-	rsync         = require('gulp-rsync'),
-	wait	      = require('gulp-wait'),
+	wait	        = require('gulp-wait'),
 	sourcemaps    = require('gulp-sourcemaps');
 
 gulp.task('browser-sync', function() {
@@ -20,23 +13,18 @@ gulp.task('browser-sync', function() {
 			baseDir: 'app'
 		},
 		notify: false,
-		// open: false,
-		// tunnel: true,
-		// tunnel: "projectname", //Demonstration page: http://projectname.localtunnel.me
-	})
+	});
 });
 
-gulp.task('styles', function() {
-	return gulp.src('app/'+syntax+'/*.'+syntax+'')
-	.pipe(wait(500))
+gulp.task('sass', function() {
+	return gulp.src('app/scss/*.scss')
+	.pipe(wait(2000))
 	.pipe(sourcemaps.init({loadMaps: true}))
-		.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
-		// .pipe(rename({ suffix: '.min', prefix : '' }))
-		.pipe(autoprefixer(['last 15 versions']))
-		// .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(autoprefixer(['last 15 versions']))
 	.pipe(sourcemaps.write())
 	.pipe(gulp.dest('app/css'))
-	.pipe(browsersync.reload( {stream: true} ))
+	.pipe(browsersync.reload( {stream: true} ));
 });
 
 gulp.task('js', function() {
@@ -44,31 +32,16 @@ gulp.task('js', function() {
 		'app/js/module.js', // Always at the end
 		])
 	.pipe(sourcemaps.init({loadMaps: true}))
-		.pipe(concat('app.min.js'))
-		//.pipe(uglify()) // Mifify js (opt.)
+	.pipe(concat('app.min.js'))
 	.pipe(sourcemaps.write())
 	.pipe(gulp.dest('app/js'))
-	.pipe(browsersync.reload({ stream: true }))
+	.pipe(browsersync.reload({ stream: true }));
 });
 
-gulp.task('rsync', function() {
-	return gulp.src('app/**')
-	.pipe(rsync({
-		root: 'app/',
-		hostname: 'username@yousite.com',
-		destination: 'yousite/public_html/',
-		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
-		recursive: true,
-		archive: true,
-		silent: false,
-		compress: true
-	}))
-});
-
-gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
-	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
-	gulp.watch(['libs/**/*.js', 'app/js/*.js'], ['js']);
-	gulp.watch('app/*.html', browsersync.reload)
+gulp.task('watch', ['sass', 'browser-sync'], function() {
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	//gulp.watch(['libs/**/*.js', 'app/js/*.js'], ['js']);
+	gulp.watch('app/*.html', browsersync.reload);
 });
 
 gulp.task('default', ['watch']);
