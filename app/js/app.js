ea15c8b302937096c3446410717b1c32cb7dd5d4
Oleg V. Anisov <anisov@obninsk.ru>
2018-11-20 15:39:22 +0300

    some correcions by review

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,37 @@
-let menu_btn = document.querySelector('#menu_btn'),
+const menu_btn = document.querySelector('#menu_btn'),
   ham_menu = document.querySelector('#ham_menu'),
+  ham_list = document.querySelector('#ham_list'),
   body = document.querySelector('body');
+  function toggleMenu(){
+    menu_btn.classList.toggle('is-active');
+    ham_menu.classList.toggle('is-active');
+    body.classList.toggle('is-locked');
+  };
 
 menu_btn.addEventListener('click', function (e) {
   e.preventDefault();
-  menu_btn.classList.toggle('is-active');
-  ham_menu.classList.toggle('is-active');
-  body.classList.toggle('is-locked');
+  toggleMenu();
 });
+ham_list.addEventListener('click', function (e) {
+  let el=e.target;
+  if (el.tagName=='A'){
+    toggleMenu();
+  };
+});
+
+
 /////////////////////////
 const slide_next=document.querySelector('#slide-next'),
       slide_prev=document.querySelector('#slide-prev'),
       slide_list=document.querySelector('#slide-list'),
       style=getComputedStyle(slide_list),
       step=parseInt(getComputedStyle(slide_list.firstElementChild).width,10),
-      //list_width=parseInt(style.width,10),
       list_width=parseInt(slide_list.children.length,10)*step;
 
+function shift_right(a,b){
+  slide_list.style.right=a+b+'px';
+  // return result=a+b+'px';
+};
 slide_next.addEventListener('click',function(e){
   e.preventDefault();
   let cur_right=parseInt(style.right,10);
@@ -25,10 +40,14 @@ slide_next.addEventListener('click',function(e){
     cur_right=0;
   };
   if (cur_right<list_width-step){
-    slide_list.style.right=cur_right+step+'px';
+    // slide_list.style.right=cur_right+step+'px';
+    // slide_list.style.right=shift(cur_right,step);
+    shift_right(cur_right,step);
   } 
   else {
-    slide_list.style.right=0+'px';
+    // slide_list.style.right=0+'px';
+    // slide_list.style.right=shift(0,0);
+    shift_right(0,0);
 }
 });
 
@@ -40,9 +59,13 @@ slide_prev.addEventListener('click',function(e){
     cur_right=0;
   };
   if (cur_right>0){
-    slide_list.style.right=cur_right-step+'px';
+    // slide_list.style.right=cur_right-step+'px';
+    // slide_list.style.right=shift(cur_right,-step);
+    shift_right(cur_right,-step);
   }
   else {
-    slide_list.style.right=list_width-step+'px';
+    // slide_list.style.right=list_width-step+'px';
+    // slide_list.style.right=shift(list_width,-step);
+    shift_right(list_width,-step);
   };
 });
