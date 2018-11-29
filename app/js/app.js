c9320aa032194b4ec379c7d5595e6006a01d02ab
Oleg V. Anisov <anisov@obninsk.ru>
2018-11-30 00:29:52 +0300

    add plugin fulpage ops

--- app/js/app.js
+++ app/js/app.js
@@ -254,3 +254,13 @@ placemarks.forEach(obj =>{
   map.geoObjects.add(placemark);
 });
 }
+//
+myFullpage = new fullpage('#fullpage', {
+  anchors: ['Page1', 'Page2', 'Page3','Page4','Page5','Page6','Page7','Page8','Page9'],
+  autoScrolling: true,
+	scrollHorizontally: true,
+	licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+  navigation: true,
+  navigationPosition: 'right',
+  scrollBar: true
+});
